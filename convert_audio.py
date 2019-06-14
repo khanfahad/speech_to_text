98ae86ddd3728c6358ab719dc452ad1221ba4e3e
Fahad Khan <khanfahad@gmail.com>
2019-06-14 03:19:59 -0500

    Create convert_audio.py

---
+++ convert_audio.py
@@ -0,0 +1,5 @@
+from pydub import AudioSegment
+
+convert mp3 file to wav                                                       
+sound = AudioSegment.from_mp3("iqbal.mp3")
+sound.export("transcript.wav", format="wav")
