7c1c36d62c45e060dec1cdce0a86d43e67cea39d
Fahad Khan <khanfahad@gmail.com>
2019-06-14 03:32:08 -0500

    Update create_audio_chunks.py

--- create_audio_chunks.py
+++ create_audio_chunks.py
@@ -9,5 +9,5 @@ chunks = make_chunks(myaudio, chunk_length_ms) #Make chunks of one sec
 
 for i, chunk in enumerate(chunks):
     chunk_name = "chunk{0}.wav".format(i)
-    print "exporting", chunk_name
+    print("exporting", chunk_name)
     chunk.export(chunk_name, format="wav")
