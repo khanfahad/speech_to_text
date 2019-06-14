ce490550ed0b3862d860f7624ad70e9ddeb08fcf
Fahad Khan <khanfahad@gmail.com>
2019-06-14 03:18:49 -0500

    Create create_audio_chunks.py

---
+++ create_audio_chunks.py
@@ -0,0 +1,13 @@
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+
+myaudio = AudioSegment.from_file("myAudio.wav" , "wav") 
+chunk_length_ms = 1000 # pydub calculates in millisec (1000ms = 1sec)
+chunks = make_chunks(myaudio, chunk_length_ms) #Make chunks of one sec
+
+#Export all of the individual chunks as wav files
+
+for i, chunk in enumerate(chunks):
+    chunk_name = "chunk{0}.wav".format(i)
+    print "exporting", chunk_name
+    chunk.export(chunk_name, format="wav")
